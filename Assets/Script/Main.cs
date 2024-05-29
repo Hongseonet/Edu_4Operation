159691faa97f8c25d780588a2f99fb33a9c108db
GENIEDEVS\hongs <hongseonmail@gmail.com>
2024-05-29 13:58:07 +0900

    # 버튼, 배경 이미지 추가 # textmesh pro 추가

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    Transform canvasRoot;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
